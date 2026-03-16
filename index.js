6c49d18e3bfa49c5942252ee26daf4cde99dd197
Aldekein <i.am@aldeke.in>
2026-03-16 16:26:03 +0800

    chore(bootstrap): polyfill Buffer and crypto random values for React Native entry
    - index.js: import react-native-get-random-values and set global.Buffer
    - package: add react-native-get-random-values and lockfile/pod updates

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+import "react-native-get-random-values"
 import "react-native-gesture-handler"
 import { AppRegistry } from "react-native"
+import { Buffer } from "buffer"
 
 import App from "./src/app/App"
 import { name as appName } from "./app.json"
 
-AppRegistry.registerComponent(appName, () => App)
+if (typeof global.Buffer === "undefined") {
+  global.Buffer = Buffer
+}
 
+AppRegistry.registerComponent(appName, () => App)
